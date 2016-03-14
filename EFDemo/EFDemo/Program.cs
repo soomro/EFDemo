c2269f00bb4dcdd6bc9f362dc81d6d075b24b0f0
Waseem Soomro <ws@waseemsoomro.com>
2016-03-14 15:15:32 +0100

    added code to create model, and then use it.
    added code to create model, and then use it.

--- EFDemo/EFDemo/Program.cs
+++ EFDemo/EFDemo/Program.cs
@@ -11,6 +11,30 @@ namespace EFDemo
         static void Main(string[] args)
         {
 
+            // Model creation
+            ModelBooks mb = new ModelBooks();
+
+            // Fetching Categories ordered by CategoryName
+            IList<Categories> categories = mb.Categories.OrderBy(o => o.CategoryName).ToList();
+
+            foreach (Categories category in categories)
+            {
+                // Writing category to to the console
+                Console.WriteLine(category.CategoryName);
+            }
+
+            //Fetching order from Orders by OrderID
+            Orders order = mb.Orders.FirstOrDefault(or => or.OrderID == 10248);
+            if (order != null)
+            {
+                foreach (Order_Details od in order.Order_Details)
+                {
+                    Console.WriteLine(od.Products.ProductName);
+                }
+            }
+            
+            Console.ReadKey();
+            
         }
     }
 }
